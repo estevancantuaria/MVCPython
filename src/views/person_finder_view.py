b4e6f7d0bc07c906a62dd60242252f442b1ad5ff
Estevan <estevancantuaria23@gmail.com>
2024-12-31 09:52:37 -0300

    feat: Adding the rest of views (need tests)

---
+++ src/views/person_finder_view.py
@@ -0,0 +1,13 @@
+from src.controllers.interfaces.person_finder_controller import PersonFinderControllerInterface
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from src.views.interfaces.view_interface import ViewInterface
+
+class PersonFinderView(ViewInterface):
+    def __init__(self, controller: PersonFinderControllerInterface) -> None:
+        self.__controller = controller
+    
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_id = http_request.query_params['person_id']
+        body_response = self.__controller.find(person_id)
+        return HttpResponse(status_code=200, body=body_response)
