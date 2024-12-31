30afa3d77cfe522560f0582b96de5a592c67a9ee
Estevan <estevancantuaria23@gmail.com>
2024-12-31 09:17:51 -0300

    feat: implementing person_creator_view (need tests)

---
+++ src/views/person_creator_view.py
@@ -0,0 +1,14 @@
+from src.controllers.person_creator_controller import PersonCreatorController
+from src.views.interfaces.view_interface import ViewInterface
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class PersonCreatorView(ViewInterface):
+    def __init__(self, controller: PersonCreatorController) -> None:
+        self.__controller = controller
+    
+    def handle(self, request: HttpRequest) -> HttpResponse:
+        person_info = request.body
+        body_response = self.__controller.create(person_info)
+        
+        return HttpResponse(body=body_response)
